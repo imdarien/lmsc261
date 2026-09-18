8eec7cf1f7440fbcce19fd53b320fcad837bea6d
imdarien <drichards8@berklee.edu>
2026-09-18 13:17:24 -0400

    Did the Frog Pond HW

---
+++ frog_pond1.js
@@ -0,0 +1,4 @@
+let numFrogs = prompt("How many frogs are about to jump in?");
+const maxFrogCapacity = 15;
+const pondIsOverCapacity = numFrogs > maxFrogCapacity
+pondIsOverCapacity ? print("beat it chud") : print("welcome big homie")
---
+++ frog_pond2.js
@@ -0,0 +1,10 @@
+const activities = ["babysit tadpoles",
+    "flies for lunch",
+    "tongue stretch",
+    "swimming lessson"];
+
+    let index = prompt("what's your activity index?")
+index = index % 4
+
+print(index)
+print(activities[index])
